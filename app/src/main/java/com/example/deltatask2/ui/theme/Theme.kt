9539117462b81db3a4f1ebfb2ca618e9f725fbb7
Sudhindra <sudhindra5678@gmail.com>
2021-05-22 13:47:17 +0530

    update Compose theme to match XML Theme

--- app/src/main/java/com/example/deltatask2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/deltatask2/ui/theme/Theme.kt
@@ -1,32 +1,20 @@
 package com.example.deltatask2.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Primary,
+    primaryVariant = Primary,
+    secondary = Secondary,
+    background = Background
 )
 
 @Composable
-fun DotsAndBoxesTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
-
+fun DotsAndBoxesTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
